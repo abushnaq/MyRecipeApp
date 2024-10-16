cc3855a58d512670d7d9befe24e0051c8fb3302c
Ahmad Bushnaq <abushnaq@gmail.com>
2024-10-16 11:11:04 -0400

    Initial Commit

---
+++ MyRecipeApp/MyRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeAppApp.swift
+//  MyRecipeApp
+//
+//  Created by Ahmad Remote on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyRecipeAppTests/MyRecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeAppTests.swift
+//  MyRecipeAppTests
+//
+//  Created by Ahmad Remote on 10/16/24.
+//
+
+import Testing
+@testable import MyRecipeApp
+
+struct MyRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
